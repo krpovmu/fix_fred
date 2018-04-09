93ca8a9343093df90589c2d1918c0763b301b2ca
krpovmu <carpov@eltiempo.com>
2018-04-08 21:36:14 -0500

    1er commit

---
+++ fred_check.py
@@ -0,0 +1,20 @@
+
+
+def check_NS
+
+def check_xalok
+
+def check_oldfred
+
+def read_file_urls(path_file):
+        f = open(path_file,"r")
+        list_urls = []
+        for line in f:
+                list_urls.append(line)
+        f.close()
+        return list_urls
+
+def write_report
+
+
+
