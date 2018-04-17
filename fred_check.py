cedbc8f10473dc9c6b46f03e8013283f3d9dcbf9
krpovmu <carpov@eltiempo.com>
2018-04-17 16:31:10 -0500

    Url Checker V0.00000001

--- fred_check.py
+++ fred_check.py
@@ -1,3 +1,9 @@
+import pycurl
+from io import BytesIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 def check_NS()
@@ -10,6 +16,17 @@ def check_elastic()
 
 
 def url_clasifier(url):
+	headers = StringIO()
+	buffer = BytesIO()
+	c = pycurl.Curl()
+	c.setopt(c.URL, url)
+	c.setopt(pycurl.WRITEFUNCTION, buffer.write)
+	c.setopt(c.HEADER, 1)
+	c.setopt(c.NOBODY, 1) # header only, no body
+	c.setopt(c.HEADERFUNCTION, headers.write)
+	c.perform()
+	c.close()
+	print headers.getvalue().split('\n')[0].split(' ')
 
 
 def read_file_urls(path_file):
